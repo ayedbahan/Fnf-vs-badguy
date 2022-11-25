aa93be80e1a688c3edc86c87be0634952c866e6a
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-11-25 21:51:19 +0300

    Add files via upload

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -25,7 +25,7 @@ import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
 #if android
-import android.Hardware;
+//import android.Hardware;
 #end
 
 using StringTools;
@@ -171,7 +171,7 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 	{
 		if(ClientPrefs.vibration)
 		{
-			Hardware.vibrate(500);
+			//Hardware.vibrate(500);
 		}
 	}
 	#end
