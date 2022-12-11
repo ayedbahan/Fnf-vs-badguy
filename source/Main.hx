926558aa774a5ae4be1d6956658749981671a082
Badguyfnf <116070144+Badguyfnf@users.noreply.github.com>
2022-12-11 22:19:03 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -86,7 +86,7 @@ class Main extends Sprite
 		SUtil.checkPermissions();
 	
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, Std.int(bool), framerate, framerate, true, false));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, Std.int(x), framerate, framerate, true, false));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
